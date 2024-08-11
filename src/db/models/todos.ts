b422a93353f31e8cbc181fba03cd26d67ea51854
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-08-11 16:21:46 +0530

    create todo schema

---
+++ src/db/models/todos.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+export default Todo;
