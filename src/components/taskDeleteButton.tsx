7725cdf30a33a7a009dd21f3b2cd97b66b58bba9
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-08-12 19:28:23 +0530

    removed alertDialog component

--- src/components/taskDeleteButton.tsx
+++ src/components/taskDeleteButton.tsx
@@ -3,8 +3,6 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import { deleteTask } from "@/lib/utils";
 import DeleteIcon from "@mui/icons-material/Delete";
